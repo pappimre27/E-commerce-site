f89e31264e2a2f5d3695834c4c7e8168100bdc7a
Papp Imre <imi950127@gmail.com>
2019-09-13 14:21:47 +0200

    Rendering collections preview

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage.jsx';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
