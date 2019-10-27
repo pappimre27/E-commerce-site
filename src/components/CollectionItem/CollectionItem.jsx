717e85d84c26adfcb22081428969915f54286410
Papp Imre <imi950127@gmail.com>
2019-10-27 18:44:39 +0100

    add to cart styling

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-buttom/custom-buttom';
 import './CollectionItem.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -11,6 +12,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton inverted> ADD to cart </CustomButton>
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,7 +31,7 @@ const Header = ({ currentUser, hidden }) => {
         )}
         <CartIcon />
       </div>
-      {hidden && <CartDropdown />}
+      {!hidden && <CartDropdown />}
     </div>
   );
 };
