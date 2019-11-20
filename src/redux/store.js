ecee184d70bbec1a6c24be6e2f4b722cfecaff9e
Papp Imre <imi950127@gmail.com>
2019-11-20 16:31:30 +0100

    moved our directory and shop data into its own reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,7 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [
-    logger
-];
+const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
